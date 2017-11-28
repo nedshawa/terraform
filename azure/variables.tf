9b4af3cd0e459afb83094951bcee1ee4471d70be
Ned Shawa <ned.shawa@gmail.com>
2017-11-28 18:39:14 +1100

    first commit

--- azure/variables.tf
+++ azure/variables.tf
@@ -1,16 +1,19 @@
 variable "resource_group" {
   description = "The name of the resource group in which to create the virtual network."
+  default = "rg-1"
 }
 
 variable "location" {
   description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
-  default     = "southcentralus"
+  default     = "australiasoutheast"
 }
 
 variable "sql_admin" {
   description = "The administrator username of the SQL Server."
+  default = "admin"
 }
 
 variable "sql_password" {
   description = "The administrator password of the SQL Server."
+  default = "admin123"
 }
