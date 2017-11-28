117f687123329e481e631c3eb44a05887dd5b800
Ned Shawa <ned.shawa@gmail.com>
2017-11-28 18:34:03 +1100

    first commit

---
+++ azure/main.tf
@@ -0,0 +1,37 @@
+provider "azurerm" {
+  subscription_id = "2ea2733f-a18f-4b5d-a61c-2652b7da9f79"
+
+  tenant_id       = "815a016b-cc6e-434e-8eff-284050d87984"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group}"
+  location = "${var.location}"
+}
+
+resource "azurerm_sql_database" "db" {
+  name                             = "mysqldatabase"
+  resource_group_name              = "${azurerm_resource_group.rg.name}"
+  location                         = "${var.location}"
+  edition                          = "Basic"
+  collation                        = "SQL_Latin1_General_CP1_CI_AS"
+  create_mode                      = "Default"
+  requested_service_objective_name = "Basic"
+  server_name                      = "${azurerm_sql_server.server.name}"
+}
+
+resource "azurerm_sql_server" "server" {
+  name                         = "${var.resource_group}-sqlsvr"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  location                     = "${var.location}"
+  version                      = "12.0"
+  administrator_login          = "${var.sql_admin}"
+  administrator_login_password = "${var.sql_password}"
+}
+
+resource "azurerm_sql_firewall_rule" "fw" {
+  name                = "firewallrules"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  server_name         = "${azurerm_sql_server.server.name}"
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
